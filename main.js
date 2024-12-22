6783f572606e243160653bdee2add12bb50c02fe
Mauricio Molina <tica.molina57@gmail.com>
2024-12-22 12:53:33 -0300

    Creo una función para el primer botón, con el evento de clic. AL presionar se creara un elemento span que contendrá el primer gato. Mediante el metodo appendChild se añade este nodo como hijo del nodo contenedor de gatos. Se agrega clickCount++, lleva la cuenta de cada evento click de este boton, asimismo de que se codifica para poner la cuenta de clics de los otros botones en 0. Se crea la condición If para que cuando el contador de clics llegue a seis, se genere un elemento span que contendrá una caja negra, la cual con el método appendChild se agregará al contenedor correspondiente, asimismo que el contenedor de los emojis quedara vacío y la cuenta de todos los clics volvera a cero

--- main.js
+++ main.js
@@ -12,3 +12,25 @@ contenedorCaja.style.display = 'inline'
 let clickCount = 0
 let clickCount1 = 0
 let clickCount2 = 0
+
+
+
+
+
+botonGato1.addEventListener('click', () => {
+    let spanCat1 = document.createElement('span')
+    spanCat1.textContent = '😺'
+    contenedorGatos.appendChild(spanCat1)
+    clickCount++;
+    clickCount1 = 0;
+    clickCount2 = 0;
+    if (clickCount === 6) {
+        let spanBlackBox = document.createElement('span')
+        spanBlackBox.textContent = '⬛'
+        contenedorCaja.appendChild(spanBlackBox)
+        contenedorGatos.textContent = ''
+        clickCount = 0;
+        clickCount1 = 0;
+        clickCount2 = 0;
+    }
+})
