e5b3fabf933ae885c4b4885096c0815428df9acb
Mauricio Molina <tica.molina57@gmail.com>
2024-12-22 12:13:11 -0300

    Selecciono los Button y Div con su respectivo id del DOM, usando query selector. En el caso de los contenedores cambio su estilo a través de javascript para que se muestren en línea, para que cuando al crearse la cantidad de gatos necesarias para crearse la caja color negra, al agregar nuevos gatos estos se muestren de manera consecutiva y no por debajo del contenedor de la caja. Preparo tres contadores para hacer seguimiento de la cantidad de clics que se presionan sobre cada uno de los gatos

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 import './style.css'
+
+const botonGato1 = document.querySelector('#btnCat1')
+const botonGato2 = document.querySelector('#btnCat2')
+const botonGato3 = document.querySelector('#btnCat3')
+
+const contenedorGatos = document.querySelector('#catConteiner')
+contenedorGatos.style.display = 'inline'
+const contenedorCaja = document.querySelector('#boxConteiner')
+contenedorCaja.style.display = 'inline'
+
+let clickCount = 0
+let clickCount1 = 0
+let clickCount2 = 0
